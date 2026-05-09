1c8704ef6cf78329a86de7e5bf6b5484cbf8f0b3
nhsbsa-wicoo <wicoo@nhsbsa.nhs.uk>
2026-05-09 22:54:03 +0100

    Update font

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("fonts");
 
   return {
     pathPrefix: "/a11y-resources/"
